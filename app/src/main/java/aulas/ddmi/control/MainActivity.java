ee0e978f5e2e9845e1ffa1f7ac0fb72aff3f740c
vpsilvay <vpsilvay@gmail.com>
2016-04-01 10:51:54 -0300

    Melhorias no CardView.

--- app/src/main/java/aulas/ddmi/control/MainActivity.java
+++ app/src/main/java/aulas/ddmi/control/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Build;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private Cursor cursor; //cusor com os dados da fonte de dados
     private RecyclerView recyclerView; //RecyclerView presente no layout associado a esta Activity
     private PlanetaAdapter adapter; //adaptador RecyclerView -> fonte de dados
+    SwipeRefreshLayout swipeRefreshLayout; //instância do swipeRefreshLayout
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +45,11 @@ public class MainActivity extends AppCompatActivity {
         adapter = new PlanetaAdapter(this, cursor, onClickPlaneta()); //cria um adaptador RecyclerView -> Fonte de dados. O Terceiro argumento é um método da Atividade que
                                                                     //implementa a interface disponibilizada no adaptador.
         recyclerView.setAdapter(adapter); //associa o adaptador a recyclerView.
+
+        //configura o SwipeRefreshLayout
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.simplerefreshlayout);
+        swipeRefreshLayout.setOnRefreshListener(OnRefreshListener());
+        swipeRefreshLayout.setColorSchemeColors(R.color.refresh_progress_1, R.color.refresh_progress_2, R.color.refresh_progress_3);
 
     }
 
@@ -67,6 +74,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent); //solicita ao Android que inicie a nova atividade
 
 
+            }
+        };
+    }
+
+    /*
+     Este método trata o evento onRefresh() do SwipeRefreshLayout.
+     Ele acontece quando o usuário faz um swipe com o dedo para baixo na View.
+    */
+    private SwipeRefreshLayout.OnRefreshListener OnRefreshListener() {
+        return new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                cursor = planetaDAO.getLista(); //obtám a lista de planetas da fonte de dados
+                adapter = new PlanetaAdapter(MainActivity.this, cursor, onClickPlaneta()); //cria um adaptador RecyclerView -> Fonte de dados. O Terceiro argumento é um método da Atividade que
+                //implementa a interface disponibilizada no adaptador.
+                recyclerView.setAdapter(adapter); //associa o adaptador a recyclerView.
+                //implementa a interface disponibilizada no adaptador.
+                recyclerView.setAdapter(adapter); //associa o adaptador a recyclerView.
+                swipeRefreshLayout.setRefreshing(false); //para a animação da swipeRefrech
             }
         };
     }
